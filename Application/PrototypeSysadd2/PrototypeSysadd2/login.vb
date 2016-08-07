601166edcbe81c13040c9e228e5a5db846127251
mmlaureta <lletty02@gmail.com>
2016-08-07 22:40:27 +0800

    080716

---
+++ Application/PrototypeSysadd2/PrototypeSysadd2/login.vb
@@ -0,0 +1,59 @@
+﻿Public Class frm_login
+
+    ' TODO: Insert code to perform custom authentication using the provided username and password 
+    ' (See http://go.microsoft.com/fwlink/?LinkId=35339).  
+    ' The custom principal can then be attached to the current thread's principal as follows: 
+    '     My.User.CurrentPrincipal = CustomPrincipal
+    ' where CustomPrincipal is the IPrincipal implementation used to perform authentication. 
+    ' Subsequently, My.User will return identity information encapsulated in the CustomPrincipal object
+    ' such as the username, display name, etc.
+
+    Private Sub OK_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles OK.Click
+        If UsernameTextBox.Text = My.Settings.Username And PasswordTextBox.Text = My.Settings.Password Then
+            frm_welcomePage.Show()
+        Else
+            MsgBox("Incorrect username or password!", MsgBoxStyle.Information, "Login failed!")
+        End If
+    End Sub
+
+    Private Sub Cancel_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Cancel.Click
+        Me.Close()
+    End Sub
+
+    Private Sub frm_login_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        'TODO: This line of code loads data into the 'DBPrototype.Login' table. You can move, or remove it, as needed.
+        Me.LoginTableAdapter.Fill(Me.DBPrototype.Login)
+
+    End Sub
+
+    Private Sub UsernameLabel_Click(ByVal sender As System.Object, ByVal e As System.EventArgs)
+
+    End Sub
+
+    Private Sub DataGridView1_CellContentClick(ByVal sender As System.Object, ByVal e As System.Windows.Forms.DataGridViewCellEventArgs)
+
+    End Sub
+
+    Private Sub BindingNavigator1_RefreshItems(ByVal sender As System.Object, ByVal e As System.EventArgs)
+
+    End Sub
+
+    Private Sub Chart1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs)
+
+    End Sub
+
+    Private Sub LoginBindingNavigatorSaveItem_Click(ByVal sender As System.Object, ByVal e As System.EventArgs)
+        Me.Validate()
+        Me.LoginBindingSource.EndEdit()
+        Me.TableAdapterManager.UpdateAll(Me.DBPrototype)
+
+    End Sub
+
+    Private Sub PasswordTextBox_TextChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles PasswordTextBox.TextChanged
+
+    End Sub
+
+    Private Sub PasswordLabel_Click(ByVal sender As System.Object, ByVal e As System.EventArgs)
+
+    End Sub
+End Class
